9b09a04a07b2ed3b534d1aec225f9fe51c40ac4e
Zach Liss <zach.liss@dockyard.com>
2021-05-14 09:32:27 -0400

    toggle image size

--- src/components/rental/image.svelte
+++ src/components/rental/image.svelte
@@ -1,7 +1,17 @@
 <script>
 	export let src, alt;
+	let isLarge = false;
+
+	function toggleSize() {
+   isLarge = !isLarge;
+  }
 </script>
 
-<div class="flex-grow-0 w-40 my-5 mx-6 text-center image">
+<button type="button"
+				class="flex-grow-0 w-40 my-5 mx-6 text-center image focus:outline-none"
+				class:w-full="{isLarge}"
+				on:click={toggleSize}>
+
   <img {src} {alt}>
-</div>
+	<small>View {isLarge ? "Smaller" : "Larger"}</small>
+</button>
