1ca3560f9a0cfddd94fea14496422c0ec570b65f
Zach Liss <zach.liss@dockyard.com>
2021-05-16 19:25:45 -0400

    update anchor hrefs to use the base path

--- src/components/nav-bar.svelte
+++ src/components/nav-bar.svelte
@@ -1,12 +1,16 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <nav class="h-16 bg-ember-primary menu">
 
-	<a sveltekit:prefetch class="pt-2 pr-10 pl-10 pb-2 text-white inline-block" href="../">
+	<a sveltekit:prefetch class="pt-2 pr-10 pl-10 pb-2 text-white inline-block" href="{base}/">
     <h1 class="italic text-3xl pt-4 pr-6">SuperRentals</h1>
 	</a>
 
   <div class="links inline-block">
-		<a sveltekit:prefetch class="pt-2 pr-10 pl-10 pb-2 text-white inline-block" href="/about">About</a>
-		<a sveltekit:prefetch class="pt-2 pr-10 pl-10 pb-2 text-white inline-block" href="/contact">Contact</a>
+		<a sveltekit:prefetch class="pt-2 pr-10 pl-10 pb-2 text-white inline-block" href="{base}/about">About</a>
+		<a sveltekit:prefetch class="pt-2 pr-10 pl-10 pb-2 text-white inline-block" href="{base}/contact">Contact</a>
   </div>
 
 </nav>
